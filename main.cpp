b648a1e923af3cbb6addad614ddc24eaac027d12
octonox <victorpetit40@gmail.com>
2018-07-09 17:48:51 +0200

    Initiaizing project

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <SFML/Graphics.hpp>
+
+int main(int argc, char* argv[])
+{
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Mars Colony");
+
+    while(window.isOpen())
+    {
+        sf::Event event;
+        while(window.pollEvent(event))
+        {
+            if(event.type == sf::Event::Closed)
+                window.close();
+        }
+        window.clear(sf::Color::White);
+        window.display();
+    }
+    return EXIT_SUCCESS;
+}
